b1b10fc71fcd7e0450ffacccc5f253cdb5d51da8
jishan1525 <jishana149@gmail.com>
2024-11-11 12:17:36 +0530

    finished the Header component

--- App.js
+++ App.js
@@ -1,10 +1,49 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+const logo = (
+  <a href="/" className="header-logo">
+    <img
+      alt="logo"
+      src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQduWeshXxyo7rGEmHnyTUsTpxOFtleP5mZ2g&s"
+    ></img>
+  </a>
+);
 
-const val = React.createElement("h1",{
-key: 1011
-},
-"I kow")
+const Header = () => 
+    (
+  <div className="header">
+    {logo}
+    <ul className="nav">
+      <a>
+        <li className="nav-item">Home</li>
+      </a>
+      <a>
+        <li className="nav-item">Cart</li>
+      </a>
+      <a>
+        <li className="nav-item">Contact</li>
+      </a>
+      <a>
+        <li className="nav-item">About</li>
+      </a>
+    </ul>
+  </div>
+);
 
+const BodyContent =()=>(
+<div className="card-content">
+    <img className="card-image" src="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/ixgxvfu6ggvw1w1awgr1"></img>
+    <p>Samosa Party</p>
+    <p>4.3 star 30-35 mins</p>
+    <p>Fast Food, Snacks, Beverages</p>
+    <p>Brigade Road</p>
+</div>
+);
+
+const Heading =() => 
+    (
+        <>
+        <Header/>
+        <BodyContent/>
+        </>
+    );
 var target = ReactDOM.createRoot(document.getElementById("header"));
-target.render(val);
+target.render(<Heading/>);
