adec84e3996dff85cd7e52a244751ce368f8c8a8
jishan1525 <jishana149@gmail.com>
2024-11-28 01:06:24 +0530

    added class based component

--- src/components/About.js
+++ src/components/About.js
@@ -1,15 +1,41 @@
 import { Outlet } from "react-router-dom";
+import React from "react";
 import Profile from "./ProfileClass";
-const About = () =>{
-return (
-    <div>
-        <h1>About Us Page</h1>
-        <p>
+// const About2 = () =>{
+// return (
+//     <div>
+//         <h1>About Us Page</h1>
+//         <p>
            
-            This is the About us page of swiggy clone</p>
-            <Profile/>
-    </div>
-)
+//             This is the About us page of swiggy clone</p>
+//             <Outlet /> {/* This is where the child route (Profile) will be rendered */}
+//     </div>
+// )
+// }
+
+class About extends React.Component{
+    constructor(props){
+        super(props)
+        console.log("Parent class const")
+    }
+    componentDidMount(){
+        console.log("Parent Mount")
+    }
+    render(){
+        console.log("Parent render")
+        return(
+
+            <div>
+            <h1>About Us Page</h1>
+            <p>
+               
+                This is the About us page of swiggy clone</p>
+                <Outlet name={"First one"}/>
+                <Profile name={"First one"}/> {/* This is where the child route (Profile) will be rendered */}
+        </div>
+        )
+    }
+
 }
 
 export default About;
--- src/components/ProfileClass.js
+++ src/components/ProfileClass.js
@@ -1,7 +1,15 @@
 import React from 'react';
 
 class Profile extends React.Component {
+  constructor(props){
+    super(props)
+    console.log("Child class const "+ this.props.name)
+}
+componentDidMount(){
+    console.log("child Mount "+ this.props.name)
+}
   render() {
+    console.log("Child render "+ this.props.name)
     return (
       <div>
         <h1>Profile component and it should be displayed when address is About/Profile</h1>
